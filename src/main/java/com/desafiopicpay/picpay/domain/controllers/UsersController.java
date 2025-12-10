8d49eb1bf048ce2cd54087826f5d764fb6c6077f
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-12-09 21:24:37 -0300

    feat: created UsersController and logic to get/post users

---
+++ src/main/java/com/desafiopicpay/picpay/domain/controllers/UsersController.java
@@ -0,0 +1,34 @@
+package com.desafiopicpay.picpay.domain.controllers;
+
+import com.desafiopicpay.picpay.domain.users.Users;
+import com.desafiopicpay.picpay.domain.users.UsersDTO;
+import com.desafiopicpay.picpay.services.UsersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    public UsersController(UsersService service){
+        this.usersService = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody UsersDTO data){
+        Users newUser = usersService.createUser(data);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Users>>getAllUsers(){
+        List<Users> listOfUsers = usersService.getAllUsers();
+        return new ResponseEntity<>(listOfUsers, HttpStatus.OK);
+    }
+}
